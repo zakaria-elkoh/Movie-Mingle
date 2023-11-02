c0b2e197d4f7b825026b82d3cb4fb1d0c6fb98bb
Echaftech23 <rachidchaf2001@gmail.com>
2023-11-02 17:14:29 +0100

    help center header & main section has been added

--- js/sign-in.js
+++ js/sign-in.js
@@ -1,1 +1,6 @@
-const showOption = document.querySelector('#hiddenItemOptionContainer'); 
+const showOption = document.querySelector('#hiddenItemOptionContainer');
+
+showOption.addEventListener('click', function(){
+    this.classList.toggle('d-none');
+    console.log(this);
+});
